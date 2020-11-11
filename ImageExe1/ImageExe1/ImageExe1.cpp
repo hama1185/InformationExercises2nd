a8af0766daf1ebaf9907ad4fe4a241db96cf26e7
hama1185 <hamataku0120@gmail.com>
2020-11-11 17:19:51 +0900

    完成一歩手前

--- ImageExe1/ImageExe1/ImageExe1.cpp
+++ ImageExe1/ImageExe1/ImageExe1.cpp
@@ -6,47 +6,111 @@
 #define M_PI 3.14159265358979 // 円周率
 #define PART 150 // 分割数
 
-void display(void)
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    int n = PART;
-    int i = PART;
+// Twitterのアイコンを作成する.
+
+void makeCircle(double x, double y, double r, double xbias, double ybias, int mode, int dose) {
     double rate;
-    double x, y, r = 0.5;
-    double xbias = 0.5;
-    double ybias = 0.25;
-    // 上胴体
+    int n = PART;
     glColor3f(1.0, 1.0, 1.0); // 描画物体に白色を設定
     glBegin(GL_POLYGON); // ポリゴンの描画
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         // 座標を計算
         rate = (double)i / n;
         x = xbias + r * cos(2.0 * M_PI * rate);
         y = ybias + r * sin(2.0 * M_PI * rate);
-        if (i <= 75) {
-            glVertex3f(x, y, 0.0); // 頂点座標を指定
+        if (mode == 0) {
+            if (i <= dose) {
+                glVertex3f(x, y, 0.0); // 頂点座標を指定
+            }
+        }
+        else {
+            if (i >= dose) {
+                glVertex3f(x, y, 0.0); // 頂点座標を指定
+            }
         }
     }
+    glEnd();
+}
 
-    // 下胴体
+void drawWing(int i) {
+    glColor3f(1.0, 1.0, 1.0); // 描画物体に白色を設定
+    glBegin(GL_POLYGON); // ポリゴンの描画　
+    if (i == 1) {
+        glVertex3f(-1.0, 0.3, 0.0);
+        glVertex3f(0.25, -0.1, 0.0);
+        glVertex3f(0.25, 0.08125, 0.0);//0.08125
+    }
+    else {
+        double x = (double)i - 1;
+        glVertex3f(-1.0, 0.3 - x * 0.2, 0.0);
+        glVertex3f(0.25, x * -0.18 + 0.08, 0.0);
+        glVertex3f(0.25, -0.1 - 0.18 * x, 0.0);//0.08125
+    }
+    glEnd();
+}
+
+void drawBackground() {
+    glBegin(GL_QUADS);
+    glColor3d(0.33, 0.67, 0.93);
+    glVertex2d(-2.0, -2.0);
+    glVertex2d(2.0, -2.0);
+    glColor3d(0.33, 0.9, 1.0);
+    glVertex2d(2.0, 2.0);
+    glVertex2d(-2.0, 2.0);
+    glEnd();
+}
+
+void drawUpBeak() {
     glColor3f(1.0, 1.0, 1.0); // 描画物体に白色を設定
     glBegin(GL_POLYGON); // ポリゴンの描画
-    r = 1.0;
-    for (i = 0; i < n; i++) {
-        // 座標を計算
-        rate = (double)i / n;
-        x = r * cos(2.0 * M_PI * rate);
-        y = r * sin(2.0 * M_PI * rate);
-        if (i >= 90) {
-            glVertex3f(x, y, 0.0); // 頂点座標を指定
-        }
-    }
-
-    // 嘴
-
-    
+    glVertex3f(1.0, 0.25, 0.0);
+    //補完
+    glVertex3f(1.25, 0.375, 0.0);
+    //補完
+    glVertex3f(1.0, 0.125, 0.0);
     glEnd();
+}
 
+void drawDownBeak() {
+    glColor3f(1.0, 1.0, 1.0); // 描画物体に白色を設定
+    glBegin(GL_POLYGON); // ポリゴンの描画
+    glVertex3f(1.0, 0.125, 0.0);
+    //補完　
+    glVertex3f(1.25, 0.25, 0.0);
+    //補完
+    glVertex3f(1.0, -0.05, 0.0);
+    glEnd();
+}
+
+void drawSpace() {
+    glColor3f(1.0, 1.0, 1.0); // 描画物体に白色を設定
+    glBegin(GL_QUADS);
+    glVertex3f(0.25, 0.08125, 0.0);
+    glVertex3f(0.25, -0.46, 0.0);
+    glVertex3f(1.0, -0.05, 0.0);
+    glVertex3f(1.0, 0.25, 0.0);
+    glEnd();
+}
+
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);//ウインドウを塗りつぶす
+    // 背景
+    drawBackground();
+    // 上胴体
+    makeCircle(0.5, 0.5, 0.4, 0.6, 0.25, 0, 86);
+    // 下胴体
+    makeCircle(0.5, 0.5, 1.0, 0, 0, 1, 100);    
+    // 上嘴
+    drawUpBeak();
+    // 下嘴
+    drawDownBeak();
+    // 羽
+    drawWing(1);
+    drawWing(2);
+    drawWing(3);
+    // 空白部分
+    drawSpace();
     glFlush();
 }
 
@@ -77,7 +141,7 @@ void keyboard(unsigned char key, int x, int y)
 
 void init(void)
 {
-    glClearColor(0.33, 0.67, 0.93, 1.0);
+    glClearColor(1.0, 1.0, 1.0, 1.0);
 }
 
 int main(int argc, char* argv[])
